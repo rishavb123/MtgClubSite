e4c56b2b63cf44d24231c1b8cb3a05d93acdab58
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-08-27 10:57:16 -0400

    added some nice js animations

--- res/js/styles.js
+++ res/js/styles.js
@@ -1,3 +1,23 @@
 if (window.innerHeight > window.innerWidth) {
     $("*").addClass("vertical-display");
 }
+
+const sections = document.getElementsByClassName("section");
+for (let i in sections) {
+    const section = sections[i];
+    $("#" + section.id).waypoint(direction => {
+        $(".nav-link").removeClass("active");
+        if (direction == "up" && sections[i - 1])
+            $("#nav-" + sections[i - 1].id).addClass("active");
+        else
+            $("#nav-" + section.id).addClass("active");
+    });
+}
+
+new SmoothScroll('a[href*="#"]', {
+    easing: "easeOutQuint",
+    speed: 500,
+    durationMin: 100,
+    updateURL: false,
+    popstate: true
+});
